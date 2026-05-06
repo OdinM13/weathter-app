df78c3f01fd731aaed674a5f4451058c14128f09
OdinM13 <markodinius@gmail.com>
2026-05-06 07:55:02 +0200

    Feat: Add renderMainValue function and small changes in css and html

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import './css/reset.css';
 import { getWeatherData } from './api.js';
 import './ui.js';
 import { filterRawData } from './logic.js';
+import { renderWeatherData } from './ui.js';
 
 
 const rawWeatherData = await getWeatherData('cologne', 'metric');
 const filteredData = filterRawData(rawWeatherData);
+renderWeatherData(filteredData);
 
 console.table(filteredData);
--- src/ui.js
+++ src/ui.js
@@ -1,2 +1,18 @@
 import './css/styles.css';
+export { renderWeatherData };
+
+function renderWeatherData(filteredData) {
+  const valueHumidity = document.querySelector(".value.humidity");
+  const valueRain = document.querySelector(".value.rain");
+  const valueWind = document.querySelector(".value.wind");
+  
+  renderMainValue(valueHumidity, filteredData.humidity);
+  renderMainValue(valueRain, filteredData.precipprob);
+  renderMainValue(valueWind, filteredData.windspeed);
+}
+
+function renderMainValue(selector, value) {
+  const content = document.createTextNode(value);
+  selector.appendChild(content);
+}
 
